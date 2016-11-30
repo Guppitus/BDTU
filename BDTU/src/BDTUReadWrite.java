715504dd10977875c04b2b07072809e26fdc4a98
Seth <seth.lunn90@gmail.com>
2016-11-30 11:58:15 -0500

    Inital class layout - not finished             Test restore

--- BDTU/src/BDTUReadWrite.java
+++ BDTU/src/BDTUReadWrite.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.IOException;
-
+import org.apache.commons.io.FileUtils;
 
 /**
  * Handles the reading and writing of file backups
@@ -9,4 +9,9 @@ import java.io.IOException;
  * Created by Seth K. Lunn on 11/30/2016.
  */
 public class BDTUReadWrite {
+
+    File file1 = FileUtils.getFile("hey");
+
+
+
 }
