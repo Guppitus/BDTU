9fd433e996c2a51252d9cc02cb1530d1d3b1255b
Seth <seth.lunn90@gmail.com>
2016-11-30 15:19:25 -0500

    Initial Backup Abstract class written

--- BDTU/src/BDTUBackUp.java
+++ BDTU/src/BDTUBackUp.java
@@ -1,8 +1,64 @@
+import java.util.Date;
+
 /**
  * Super Class containing Parent Backup Structure
  *
  *
  * Created by Seth K. Lunn on 11/30/2016.
  */
-public class BDTUBackUp {
+public abstract class BDTUBackUp {
+
+    private String source;
+    private String destination;
+    private long size;
+
+//  TODO 7zip and network
+    private boolean encrytpion;
+    private boolean network;
+
+//  TODO session backup
+    private String netDesitination;
+    private Date creationDate;
+    private Date modificationDate;
+    private Date sesssionLength;
+    private boolean recurring;
+    private int recurringValue;
+
+
+    public BDTUBackUp(String source, String destination, long size) {
+
+        System.out.println("creating backup record");
+        this.source = source;
+        this.destination = destination;
+        this.size = size;
+
+    }
+
+    public String toString() {
+        return source + " " + destination + " " + size;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long newSize) {
+        size = newSize;
+    }
+
+    public void setSource(String newSource) {
+        source = newSource;
+    }
+
+    public void setDestination(String newDestination) {
+        destination = newDestination;
+    }
 }
